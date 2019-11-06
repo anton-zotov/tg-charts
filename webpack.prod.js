0b3fab7c09e456ab79d17e9351654998e6b8bd12
Anton Zotov <anton.a.zotov@gmail.com>
2019-11-06 18:38:14 +0100

    webpack html plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'production',
@@ -38,5 +39,8 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	plugins: [new HtmlWebpackPlugin({
+		title: 'Zero dependency chart'
+	})]
 };
